718e46e0f8e985567183fdc807fece1817f522a6
Escalante <facudnoesclante@gmail.com>
2025-06-27 11:40:40 -0300

    conexion a la base de datos exitosa

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,7 @@
 
 import { Sequelize } from 'sequelize';
-export const sequelize = new Sequelize({
-  dialect: 'mysql',
-  server: "localhost",
-  port: 3306,
-  database: "books",
-  authentication: {
-    type: 'default',
-    options: {
-      userName: "root",
-      password: ""
-    },
-  },
-}
 
-);
-
+export const sequelize = new Sequelize('books', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
